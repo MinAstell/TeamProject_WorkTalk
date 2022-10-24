a87a29b13b0e0a167a54b044515e75f121dc5016
GyeongMin Baek <speekup2022@gmail.com>
2022-10-24 20:28:23 +0900

    BKM 1 update

---
+++ app/src/main/java/com/bkm/worktalk/Fragment.java
@@ -0,0 +1,52 @@
+package com.bkm.worktalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Fragment extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private FragProject fragProject;
+    private FragTalk fragTalk;
+    private FragSettings fragSettings;
+    private FragmentTransaction transaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        fragmentManager = getSupportFragmentManager();
+
+        fragProject = new FragProject();
+        fragTalk = new FragTalk();
+        fragSettings = new FragSettings();
+
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.framelayout, fragProject).commitAllowingStateLoss();
+
+        findViewById(R.id.iv_project).setOnClickListener(mClick);
+        findViewById(R.id.iv_talk).setOnClickListener(mClick);
+        findViewById(R.id.iv_settings).setOnClickListener(mClick);
+    }
+    View.OnClickListener mClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            transaction = fragmentManager.beginTransaction();
+            switch(v.getId()) {
+                case R.id.iv_project:
+                    transaction.replace(R.id.framelayout, fragProject).commitAllowingStateLoss();
+                    break;
+                case R.id.iv_talk:
+                    transaction.replace(R.id.framelayout, fragTalk).commitAllowingStateLoss();
+                    break;
+                case R.id.iv_settings:
+                    transaction.replace(R.id.framelayout, fragSettings).commitAllowingStateLoss();
+                    break;
+            }
+        }
+    };
+}
---
+++ app/src/main/java/com/bkm/worktalk/Login_Join.java
@@ -0,0 +1,14 @@
+package com.bkm.worktalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Join extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_join);
+    }
+}
